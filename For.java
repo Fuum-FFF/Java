d2079f62df8b5b3add973629744db85274093120
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-05 22:00:57 +0900

    for文に関して

---
+++ For.java
@@ -0,0 +1,12 @@
+public class For{
+    public static void main(String[] args){
+        int j = 5;
+        int ans = 1;
+
+        //2の5乗
+        for(int y = 1;y <= j;y++){
+            ans = ans * 2;
+        }
+        System.out.println(ans);
+    }
+}
