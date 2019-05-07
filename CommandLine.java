a2142c4144826d0513d55e43c1afa44b8015b09c
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-07 21:56:10 +0900

    コマンドライン

---
+++ CommandLine.java
@@ -0,0 +1,10 @@
+public class CommandLine{
+/*(String[] args)を
+(String[] aaa)に変更しても
+実行結果は変わらない。*/
+    public static void main(String[] aaa){
+        for(int i = 0;i < aaa.length;i++){
+            System.out.println(aaa[i]);
+        }
+    }
+}
