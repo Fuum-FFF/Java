6d0c4b9b3510604189326dfe35bcea05e59ec3c5
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-13 08:12:51 +0900

    小数点リテラル

---
+++ SyosutenLiteral_Re.java
@@ -0,0 +1,19 @@
+public class SyosutenLiteral{
+    public static void main(String[] args){
+        /*
+        浮動小数点リテラルの書き方
+        */
+
+        //32ビットはf・F
+        System.out.println(12.345f);
+
+        //64ビットはd・D及び何もない
+        System.out.println(12.345);
+
+        //32ビット
+        System.out.println(2.1e2f);
+
+        //64ビット
+        System.out.println(2.1e2);
+    }
+}
