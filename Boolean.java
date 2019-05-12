698dc2aace167783eb4cdc19999276635c42830c
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-13 08:20:05 +0900

    boolean型

---
+++ Boolean.java
@@ -0,0 +1,12 @@
+public class Boolean{
+    public static void main(String[] args){
+        System.out.println(true);
+        System.out.println(false);
+
+        /*
+        System.out.println(True);
+        System.out.println(FALSE);
+        上記は絶対ダメ
+        */
+    }
+}
