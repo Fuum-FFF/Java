c0bcd13b11873bf73425ec0a4c7362388ffcf4b9
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-13 07:39:56 +0900

    〇進数

---
+++ Literal.java
@@ -0,0 +1,16 @@
+public class Literal{
+    public static void main(String[] args){
+        //2進数
+        System.out.println(0b1000001);
+        //8進数
+        System.out.println(0101);
+        //10進数
+        System.out.println(65);
+        //16進数
+        System.out.println(0x41);
+        //32ビット
+        System.out.println(0);
+        //64ビット
+        System.out.println(0L);
+    }
+}
