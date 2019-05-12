5d9f04e1cca582933466ad365455621069fa6093
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-13 08:07:22 +0900

    小数点リテラル

---
+++ MojiLiteral.java
@@ -0,0 +1,9 @@
+public class MojiLiteral{
+    public static void main(String[] args){
+        //文字「A」を出力。
+        System.out.println('A');
+
+        //文字コードが16進数の0041に対応する文字を出力。
+        System.out.println('\u0041');
+    }
+}
