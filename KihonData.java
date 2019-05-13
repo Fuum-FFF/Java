569a3fff3cbcef2d891ea39c59e246526fe7afdf
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-14 08:10:30 +0900

    基本データ型

---
+++ KihonData.java
@@ -0,0 +1,28 @@
+public class KihonData{
+    public static void main(String[] args){
+        //整数型変数
+        byte b = 10;
+        short s = 11;
+        int i = 12;
+        long l = 13L;
+
+        //小数型変数
+        float f = 12.34f;
+        double d = 56.78;
+
+        //文字型変数
+        char c = 'A';
+
+        //boolean型変数
+        boolean bl = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bl);
+    }
+}
