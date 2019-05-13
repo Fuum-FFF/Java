44497389eb976c9b3ad93eec4eb88afc8d524095
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-14 07:37:12 +0900

    文字リテラルその2

---
+++ MojiLiteral2.java
@@ -0,0 +1,9 @@
+public class MojiLiteral2{
+    public static void main(String[] args){
+        System.out.println("ABC");
+        System.out.println("あ");
+        System.out.println("");
+        //"XYZ"と出力
+        System.out.println("\"XYZ\"");
+    }
+}
