9eb2c22cf978c1023d150996599e5f59ff2607a9
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-14 07:47:07 +0900

    ※コメントアウトを外したらエラーになります。

---
+++ Ketakugiri.java
@@ -0,0 +1,13 @@
+public class Ketakugiri{
+    public static void main(String[] args){
+        System.out.println(0b1111_1111);
+        System.out.println(0_377);
+        System.out.println(0_5_5);
+        System.out.println(0b1111____1111);
+
+        //System.out.println(120_);
+        //System.out.println(12._34);
+        //System.out.println(255_L);
+        //System.out.println(0b_1111);
+    }
+}
