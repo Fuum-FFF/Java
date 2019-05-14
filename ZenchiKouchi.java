8c0a76f78a0c64d7591b52d88e0a626257820556
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-15 08:12:38 +0900

    インクリメント関係

---
+++ ZenchiKouchi.java
@@ -0,0 +1,11 @@
+public class ZenchiKouchi{
+    public static void main(String[] args){
+        //前置インクリメント
+        int x = 0;
+        System.out.println(++x);
+
+        //後置インクリメント
+        int y = 0;
+        System.out.println(y++);
+    }
+}
