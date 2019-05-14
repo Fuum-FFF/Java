d8abbbe485dfd5ce870c67f22ea2d78f4ed6e3f7
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-15 08:04:48 +0900

    複合代入演算子

---
+++ FukugoudainyuEnzanshi.java
@@ -0,0 +1,15 @@
+public class FukugoudainyuEnzanshi{
+    public static void main(String[] args){
+        int x = 8;
+        x += 2;
+        System.out.println(x);
+
+        byte y = 2;
+        y /= 3;
+        System.out.println(y);
+
+        double z = 10.5;
+        z += 3.2;
+        System.out.println(z);
+    }
+}
