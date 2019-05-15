2a33c67ae6301541d3a943fe6ee50736dc7722aa
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-16 08:07:06 +0900

    Switch文

---
+++ Switchbun.java
@@ -0,0 +1,16 @@
+public class Switchbun{
+    public static void main(String[] args){
+        int num = 1;
+        switch(++num){
+            case 1:
+            System.out.println(1);
+            case 2:
+            System.out.println(2);
+            case 3:
+            System.out.println(3);
+            break;
+            default:
+            System.out.println(4);
+        }
+    }
+}
