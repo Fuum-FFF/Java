906a4c3ddddac2ea8d78c7e336018094d27a0437
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-18 18:37:49 +0900

    DoWhile文

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile{
+    public static void main(String[] args){
+        int x = 0;
+        do{
+            System.out.println(10101);
+            x++;
+        }while(x <= 10);
+    }
+}
