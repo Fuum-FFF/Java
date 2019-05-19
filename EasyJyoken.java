35ea9a2a84d3ca5539c8abe50f3dcdc4140ecb56
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-19 22:15:19 +0900

    簡単なif文

---
+++ EasyJyoken.java
@@ -0,0 +1,8 @@
+public class EasyJyoken{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 7;
+        int ans = (x > y) ? x : y;
+        System.out.println(ans);
+    }
+}
