d2055901e40a8dad1e16f818177469e48eb05a72
fuum1813 <kntshsgw11181813@gmail.com>
2019-05-20 08:07:23 +0900

    二次元配列その2

---
+++ Nijigenhairetsu2.java
@@ -0,0 +1,11 @@
+public class Nijigenhairetsu2{
+    public static void main(String[] args){
+        int a[][] = {{0,1,2},{3,4,5}};
+
+        for(int x = 0; x < 2;x++){
+            for(int b = 0; b < 3;b++){
+                System.out.println(a[x][b]);
+            }
+        }
+     }
+}
