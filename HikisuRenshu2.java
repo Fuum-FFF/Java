883f252de8f6be3b8a63c9a303824757e0f94c76
fuum1813 <kntshsgw11181813@gmail.com>
2019-06-11 08:26:14 +0900

    引数の練習その2

---
+++ HikisuRenshu2.java
@@ -0,0 +1,11 @@
+class HikisuRenshu2{
+    public static void main(String[] args){
+        //文字
+        printData(100,"りんご");
+    }
+ public static void printData(int i,String s){
+     //出力
+     System.out.print(i);
+     System.out.println(s);
+ } 
+}
